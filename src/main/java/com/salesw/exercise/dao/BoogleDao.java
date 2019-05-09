aa9049c5d20368c046fdd71eb7069a6a21758413
manjeersrujan <manjeer.srujan@gmail.com>
2019-05-09 21:38:39 +0800

    Adding heroku redis

--- src/main/java/com/salesw/exercise/dao/BoogleDao.java
+++ src/main/java/com/salesw/exercise/dao/BoogleDao.java
@@ -3,6 +3,15 @@ package com.salesw.exercise.dao;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.redisson.Redisson;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.stereotype.Component;
 
 import com.salesw.exercise.model.BoogleBoard;
@@ -12,11 +21,30 @@ public class BoogleDao {
 
 	private Map<Long, BoogleBoard> map = new HashMap<>();
 
+	private final String BOOGLE_MAP = "BOOGLE_MAP";
+
+	@Autowired
+	SalesWRedissonClient salesWRedissonClient;
+
 	public void save(BoogleBoard boogleBoard) {
+
+		if (salesWRedissonClient != null && salesWRedissonClient.getClient() != null) {
+			RMap<Long, BoogleBoard> map = salesWRedissonClient.getClient().getMap(BOOGLE_MAP);
+			map.put(boogleBoard.getId(), boogleBoard);
+			return;
+		}
+
 		map.put(boogleBoard.getId(), boogleBoard);
+
 	}
 
 	public BoogleBoard get(long id) {
+
+		if (salesWRedissonClient != null && salesWRedissonClient.getClient() != null) {
+			RMap<Long, BoogleBoard> map = salesWRedissonClient.getClient().getMap(BOOGLE_MAP);
+			return map.get(id);
+		}
+
 		return map.get(id);
 	}
 
--- src/main/java/com/salesw/exercise/main/Application.java
+++ src/main/java/com/salesw/exercise/main/Application.java
@@ -8,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-
-
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
